6a1ecac2124b0d9747d4e2a5723c756f81057e99
Tedpi1 <moroted16@gmail.com>
2025-09-16 14:51:55 +0300

    c++ day 2

---
+++ math.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    cout<<"Square root of 16: "<<sqrt(16)<<endl;
+    cout<<"Power of 2^3: "<<pow(2,3)<<endl;
+    cout<<"Round of 4.5: "<<round(4.5)<<endl;
+    cout<<"Logarithm of 2`: "<<log(2)<<endl;
+}
